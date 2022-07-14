7a2390e3cfd20564d89cbf99391c8654f55d5a7a
Isha Gupta <56040878+ishagupta6714@users.noreply.github.com>
2022-07-14 23:14:09 +0530

    Create levelorder.java

---
+++ levelorder.java
@@ -0,0 +1,13 @@
+public static void levelOrder(Node root) {
+    Queue<Node> q=new LinkedList<>();
+    q.add(root);
+    while(!q.isEmpty()){
+        Node temp=q.remove();
+        System.out.print(temp.data+" ");
+        if(temp.left!=null)
+        q.add(temp.left);
+        if(temp.right!=null)
+        q.add(temp.right);
+    }
+      
+    }
