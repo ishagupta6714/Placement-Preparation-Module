165816ad1be68250b6b9fff9fac71de38098b132
Isha Gupta <56040878+ishagupta6714@users.noreply.github.com>
2022-08-08 17:31:08 +0530

    Create topological_sort.java

---
+++ topological_sort.java
@@ -0,0 +1,16 @@
+static boolean check(ArrayList<ArrayList<Integer>> list, int V, int[] res) {
+        
+        if(V!=res.length)
+        return false;
+        
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res[i]] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : list.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
